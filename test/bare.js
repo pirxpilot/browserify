891bd0cffc8ed220283dbaee8b2f071dd5308033
Damian Krzeminski <pirxpilot@furkot.com>
2018-12-21 11:59:45 -0700

    stop using deprecated Buffer constructor

--- test/bare.js
+++ test/bare.js
@@ -12,19 +12,21 @@ var tmpdir = temp.mkdirSync({prefix: 'browserify-test'});
 
 test('bare', function (t) {
     t.plan(4);
-    
+
     var cwd = process.cwd();
     process.chdir(__dirname);
-    
+
     var ps = spawn(process.execPath, [
         path.resolve(__dirname, '../bin/cmd.js'),
         '-', '--bare'
     ]);
     ps.stdout.pipe(concat(function (body) {
         vm.runInNewContext(body, {
-            Buffer: function (s) { return s.toLowerCase() },
+            Buffer: {
+                from: function (s) { return s.toLowerCase(); }
+            },
             console: {
-                log: function (msg) { t.equal(msg, 'abc') }
+                log: function (msg) { t.equal(msg, 'abc'); }
             }
         });
         vm.runInNewContext(body, {
@@ -32,13 +34,13 @@ test('bare', function (t) {
             console: {
                 log: function (msg) {
                     t.ok(Buffer.isBuffer(msg));
-                    t.equal(msg.toString('utf8'), 'ABC')
+                    t.equal(msg.toString('utf8'), 'ABC');
                 }
             }
         });
     }));
-    ps.stdin.end('console.log(Buffer("ABC"))');
-    
+    ps.stdin.end('console.log(Buffer.from("ABC"))');
+
     ps.on('exit', function (code) {
         t.equal(code, 0);
     });
@@ -46,14 +48,16 @@ test('bare', function (t) {
 
 test('bare api', function (t) {
     t.plan(3);
-    
+
     var input = through();
     var b = browserify(input, { bare: true });
     b.bundle().pipe(concat(function (body) {
         vm.runInNewContext(body, {
-            Buffer: function (s) { return s.toLowerCase() },
+            Buffer: {
+              from: function (s) { return s.toLowerCase(); }
+            },
             console: {
-                log: function (msg) { t.equal(msg, 'abc') }
+                log: function (msg) { t.equal(msg, 'abc'); }
             }
         });
         vm.runInNewContext(body, {
@@ -61,24 +65,24 @@ test('bare api', function (t) {
             console: {
                 log: function (msg) {
                     t.ok(Buffer.isBuffer(msg));
-                    t.equal(msg.toString('utf8'), 'ABC')
+                    t.equal(msg.toString('utf8'), 'ABC');
                 }
             }
         });
     }));
-    input.end('console.log(Buffer("ABC"))');
+    input.end('console.log(Buffer.from("ABC"))');
 });
 
 test('bare inserts __filename,__dirname but not process,global,Buffer', function (t) {
     t.plan(2);
-    
+
     var file = path.resolve(__dirname, 'bare/main.js');
     var ps = spawn(process.execPath, [
         path.resolve(__dirname, '../bin/cmd.js'),
         file,
         '--bare'
     ]);
-    
+
     ps.stdout.pipe(concat(function (body) {
         vm.runInNewContext(body, {
             require: require,
@@ -97,7 +101,7 @@ test('bare inserts __filename,__dirname but not process,global,Buffer', function
         });
     }));
     ps.stdin.end();
-    
+
     ps.on('exit', function (code) {
         t.equal(code, 0);
     });
@@ -105,7 +109,7 @@ test('bare inserts __filename,__dirname but not process,global,Buffer', function
 
 test('bare inserts dynamic __filename,__dirname', function (t) {
     t.plan(2);
-    
+
     var file = path.join(tmpdir, 'dirname-filename.js');
 
     fs.writeFileSync(
@@ -118,7 +122,7 @@ test('bare inserts dynamic __filename,__dirname', function (t) {
         file,
         '--bare'
     ]);
-    
+
     ps.stdout.pipe(concat(function (body) {
         vm.runInNewContext(body, {
             require: require,
@@ -134,7 +138,7 @@ test('bare inserts dynamic __filename,__dirname', function (t) {
         });
     }));
     ps.stdin.end();
-    
+
     ps.on('exit', function (code) {
         t.equal(code, 0);
     });
@@ -142,7 +146,7 @@ test('bare inserts dynamic __filename,__dirname', function (t) {
 
 test('bare inserts dynamic __filename,__dirname with basedir', function (t) {
     t.plan(2);
-    
+
     var file = 'dirname-filename.js';
     var ps = spawn(process.execPath, [
         path.resolve(__dirname, '../bin/cmd.js'),
@@ -150,7 +154,7 @@ test('bare inserts dynamic __filename,__dirname with basedir', function (t) {
         '--bare',
         '--basedir=' + path.join(__dirname, 'bare')
     ]);
-    
+
     ps.stdout.pipe(concat(function (body) {
         vm.runInNewContext(body, {
             require: require,
@@ -166,7 +170,7 @@ test('bare inserts dynamic __filename,__dirname with basedir', function (t) {
         });
     }));
     ps.stdin.end();
-    
+
     ps.on('exit', function (code) {
         t.equal(code, 0);
     });
